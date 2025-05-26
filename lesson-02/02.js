0e6ef99f632f80751b70213c7eba3193718aa115
Sergey <O.l.h@yandex.ru>
2025-05-26 15:51:44 +0300

    Complete1

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -1,15 +1,4 @@
-let dice1 = Math.floor(Math.random() * 6) + 1 // случайное число от 1 до 6
-let dice2 = Math.floor(Math.random() * 6) + 1 // случайное число от 1 до 6
+let dice1 = Math.floor(Math.random() * 6) + 1;
+let dice2 = Math.floor(Math.random() * 6) + 1;
 
-let isWinningDouble = (dice1 === dice2) && (dice1 > 3)
-
-// Проверка (раскомментируйте для вывода в консоль)
-/*
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
-} else {
-  console.log('Не выигрышный дубль.')
-}
-*/
+let isWinningDouble = (dice1 === dice2) && (dice1 > 3);
