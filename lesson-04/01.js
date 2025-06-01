07c46b368a101cacbf4879e01654d3ef9c8fc381
Sergey <O.l.h@yandex.ru>
2025-06-01 18:41:38 +0300

    Complete1

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -15,3 +15,13 @@ function findCommonElements(array1, array2) {
 
 // Пример использования:
 console.log(findCommonElements([1, 2, 3], [2, 3, 4])); // выведет [2, 3]
+
+// --- Ниже пример, как правильно объявлять переменную score ---
+
+let score = 0;  // ОБЯЗАТЕЛЬНО объявляем переменную перед использованием
+
+for (let i = 0; i < 5; i++) {
+    score += i;
+}
+
+console.log('Score:', score);
