fd85b15778af4f2057c79e6686f637f9584b9832
Sergey <O.l.h@yandex.ru>
2025-06-01 19:10:33 +0300

    Complete1

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -6,3 +6,7 @@ function includesElement(array, element) {
     }
     return false; // элемент не найден
 }
+
+// Пример использования:
+console.log(includesElement([1, 2, 3], 2)); // true
+console.log(includesElement([1, 2, 3], 5)); // false
