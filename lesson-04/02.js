8313f4bf34eb7d321ed51f07841e35595f1bbff8
Sergey <O.l.h@yandex.ru>
2025-06-01 18:53:26 +0300

    Complete1

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -1,17 +1,24 @@
+function includesElement(array, element) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            return true;
+        }
+    }
+    return false;
+}
+
 function findUniqueElements(array) {
-    let uniqueArray = []; // создаём пустой массив для уникальных элементов
+    let uniqueArray = [];
 
-    for (let i = 0; i < array.length; i++) { // перебираем все элементы входного массива
+    for (let i = 0; i < array.length; i++) {
         let currentElement = array[i];
-
-        // проверяем, есть ли currentElement в uniqueArray
         if (!includesElement(uniqueArray, currentElement)) {
-            uniqueArray.push(currentElement); // если нет, добавляем его в uniqueArray
+            uniqueArray.push(currentElement);
         }
     }
 
-    return uniqueArray; // возвращаем массив с уникальными элементами
+    return uniqueArray;
 }
 
 // Пример использования:
-console.log(findUniqueElements([1, 2, 3, 2, 1, 4])); // выведет [1, 2, 3, 4]
+console.log(findUniqueElements([1, 2, 3, 2, 1, 4])); // [1, 2, 3, 4]
