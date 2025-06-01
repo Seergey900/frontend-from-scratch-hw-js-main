aaf1dd53e74d8d58c4dbea885e44907c7c24fcf2
Sergey <O.l.h@yandex.ru>
2025-06-01 18:50:58 +0300

    Complete1

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -1,27 +1,12 @@
-function findCommonElements(array1, array2) {
-    // Создаем множество из второго массива для быстрого поиска
-    const set2 = new Set(array2);
-    const commonSet = new Set();
-
-    for (const element of array1) {
-        if (set2.has(element)) {
-            commonSet.add(element); // добавляем в множество общих элементов
+function includesElement(array, element) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            return true; // элемент найден
         }
     }
-
-    // Преобразуем множество обратно в массив и возвращаем
-    return Array.from(commonSet);
+    return false; // элемент не найден
 }
 
 // Пример использования:
-console.log(findCommonElements([1, 2, 3], [2, 3, 4])); // выведет [2, 3]
-
-// --- Ниже пример, как правильно объявлять переменную score ---
-
-let score = 0;  // ОБЯЗАТЕЛЬНО объявляем переменную перед использованием
-
-for (let i = 0; i < 5; i++) {
-    score += i;
-}
-
-console.log('Score:', score);
+console.log(includesElement([1, 2, 3], 2)); // true
+console.log(includesElement([1, 2, 3], 5)); // false
